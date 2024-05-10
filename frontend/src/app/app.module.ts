4419541914df6a42fdce280e0127035597116449
Meco <mecodine15@gmail.com>
2024-05-10 20:29:51 +0200

    adding primeng and login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import {CardModule} from "primeng/card";
+import {FloatLabelModule} from "primeng/floatlabel";
+import {ChipsModule} from "primeng/chips";
+import {ButtonModule} from "primeng/button";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    FloatLabelModule,
+    ChipsModule,
+    ButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
