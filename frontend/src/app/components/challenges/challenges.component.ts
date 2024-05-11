9daa083ae635e057f6bbbcb1bf02ea959ad79a81
Meco <mecodine15@gmail.com>
2024-05-11 02:41:29 +0200

    completing the ui for leaderboard and some fixes for challenges view

--- frontend/src/app/components/challenges/challenges.component.ts
+++ frontend/src/app/components/challenges/challenges.component.ts
@@ -22,4 +22,8 @@ export class ChallengesComponent {
       // width: '70%'
     });
   }
+
+  handleFixedButtonClick() {
+
+  }
 }
--- frontend/src/app/components/leaderboard/leaderboard.component.ts
+++ frontend/src/app/components/leaderboard/leaderboard.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
+interface Column {
+  field: string;
+  header: string;
+}
+
+interface Product {}
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
@@ -7,13 +13,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LeaderboardComponent implements OnInit{
   data: any;
-
   options: any;
+  data1: any;
+  options1: any;
+
+  products!: Product[];
+
+  cols!: Column[];
+
+  // constructor(private productService: ProductService) {}
+
+  // ngOnInit() {
+  //
+  // }
 
   ngOnInit() {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
     const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
     this.data = {
       labels: ['Java', 'Python', 'Javascript', 'Go', 'Php', 'C++', 'Assembly'],
@@ -45,6 +63,156 @@ export class LeaderboardComponent implements OnInit{
           },
           pointLabels: {
             color: textColorSecondary
+          }
+        }
+      }
+    };
+
+    this.products = [
+      {
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        description: 'Product Description',
+        image: 'bamboo-watch.jpg',
+        price: 65,
+        category: 'Accessories',
+        quantity: 24,
+        inventoryStatus: 'INSTOCK',
+        rating: 5
+      },
+      {
+        id: '2000',
+        code: 'as3as4as34asa',
+        name: 'Black Watch',
+        description: 'Product Description',
+        image: 'black-watch.jpg',
+        price: 72,
+        category: 'Accessories',
+        quantity: 61,
+        inventoryStatus: 'INSTOCK',
+        rating: 4
+      },
+      {
+        id: '3000',
+        code: 'asht34t3g3',
+        name: 'Blue Band',
+        description: 'Product Description',
+        image: 'blue-band.jpg',
+        price: 79,
+        category: 'Fitness',
+        quantity: 2,
+        inventoryStatus: 'LOWSTOCK',
+        rating: 3
+      },
+      {
+        id: '4000',
+        code: 'sdf43534t3',
+        name: 'Blue T-Shirt',
+        description: 'Product Description',
+        image: 'blue-t-shirt.jpg',
+        price: 29,
+        category: 'Clothing',
+        quantity: 25,
+        inventoryStatus: 'INSTOCK',
+        rating: 5
+      },
+      {
+        id: '5000',
+        code: 'treg34rth',
+        name: 'Bracelet',
+        description: 'Product Description',
+        image: 'bracelet.jpg',
+        price: 15,
+        category: 'Accessories',
+        quantity: 73,
+        inventoryStatus: 'INSTOCK',
+        rating: 4
+      },
+      {
+        id: '6000',
+        code: 'fg834r',
+        name: 'Brown Purse',
+        description: 'Product Description',
+        image: 'brown-purse.jpg',
+        price: 120,
+        category: 'Accessories',
+        quantity: 0,
+        inventoryStatus: 'OUTOFSTOCK',
+        rating: 4
+      },
+      {
+        id: '7000',
+        code: 't8f8u4t8t',
+        name: 'Chakra Bracelet',
+        description: 'Product Description',
+        image: 'chakra-bracelet.jpg',
+        price: 32,
+        category: 'Accessories',
+        quantity: 5,
+        inventoryStatus: 'LOWSTOCK',
+        rating: 3
+      },
+      {
+        id: '8000',
+        code: 'stht4w35t',
+        name: 'Galaxy Earrings',
+        description: 'Product Description',
+        image: 'galaxy-earrings.jpg',
+        price: 34,
+        category: 'Accessories',
+        quantity: 23,
+        inventoryStatus: 'INSTOCK',
+        rating: 5
+      }
+    ];
+
+    this.cols = [
+      { field: 'code', header: 'Code' },
+      { field: 'name', header: 'Name' },
+      { field: 'category', header: 'Category' },
+      { field: 'quantity', header: 'Quantity' }
+    ];
+    this.data1 = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Your Progress',
+          data: [3, 0, 6, 1, 2, 1, 3],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          tension: 0.4
+        },
+      ]
+    };
+
+    this.options1 = {
+      maintainAspectRatio: false,
+      aspectRatio: 1,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
           }
         }
       }
